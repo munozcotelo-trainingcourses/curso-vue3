fa875e750247fff696ffd07a01a11f1e0b710266
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2022-07-19 10:11:09 +0200

    Template refs - Acceder a un componente de vue

--- src/components/TemplateRef.vue
+++ src/components/TemplateRef.vue
@@ -2,9 +2,19 @@
 
     import * as vue from "vue";
 
+    import TemplateRefHijo1 from "./TemplateRefHijo1.vue";
+    import TemplateRefHijo2 from "./TemplateRefHijo2.vue";
+
     export default vue.defineComponent( {
 
         name : "TemplateRef",
+
+        components : {
+
+            TemplateRefHijo1 : TemplateRefHijo1,
+            TemplateRefHijo2 : TemplateRefHijo2,
+
+        },
 
         created() : void {
             console.info( "TemplateRef", this.$refs );
@@ -15,6 +25,11 @@
             doFocus() : void {
 
                 this.$refs.myInputRef.focus();
+                console.info( "Hijo1", this.$refs.miHijo1 );
+                console.info( "Hijo2", this.$refs.miHijo2 );
+                this.$refs.miHijo1.doClick();
+                this.$refs.miHijo2.doClick();
+
             },
 
         },
@@ -31,6 +46,12 @@
 
         <button v-on:click="doFocus">Poner foco</button>
         <input type="text" name="ref-input" value="" ref="myInputRef"/>
+
+        <div>
+            <p>Por consola se pueden ver los hijos tras pulsar en el boton "Poner Foco"</p>
+            <TemplateRefHijo1 ref="miHijo1" />
+            <TemplateRefHijo2 ref="miHijo2"/>
+        </div>
 
     </div>
 
---
+++ src/components/TemplateRefHijo2.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+
+    import * as vue from "vue";
+
+    export default vue.defineComponent( {
+
+        name : "TemplateRefHijo2",
+
+        data() : Record<string, unknown> {
+
+            return {
+
+                miDato: 100 as number,
+                otroDato: 10 as number,
+
+            };
+
+        },
+
+        expose : [ "miDato", "doClick" ],
+
+        methods: {
+
+            doClick() : void {
+
+                this.miDato++;
+            },
+
+        },
+
+    } );
+
+</script>
+
+<template>
+
+    <div class="component">
+
+        <h2>TemplateRef Hijo2</h2>
+
+        <p>Mi dato {{ miDato }}</p>
+        <button v-on:click="doClick">Click</button>
+
+    </div>
+
+</template>
+
+
+
